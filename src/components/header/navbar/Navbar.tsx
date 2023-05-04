58ab4dc1889d30f798992bec695b62c432181b29
matvey84 <matvey8407@mail.ru>
2023-05-04 12:45:59 +0300

    feat: add header

---
+++ src/components/header/navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './navbar.scss';
+
+function Navbar() {
+  return (
+    <nav className="header_navbar">
+      <NavLink className={'header_navbar__link'} to={'/'}>
+        Поиск Вакансий
+      </NavLink>
+      <NavLink className={'header_navbar__link'} to={'/favorite-vacancy/'}>
+        Избранное
+      </NavLink>
+    </nav>
+  );
+}
+export default Navbar;
