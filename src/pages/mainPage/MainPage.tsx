ef34aa439b5b4b9d4362ae65c6ec4e98069810e1
matvey84 <matvey8407@mail.ru>
2023-05-04 12:46:32 +0300

    fet: add main page

---
+++ src/pages/mainPage/MainPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function MainPage() {
+  return (
+    <section className="page main-page">
+      <div className="container">
+        <aside className="main-page_form-block"></aside>
+        <section>
+          <input type="text" />
+          поиск вакансий
+        </section>
+      </div>
+    </section>
+  );
+}
+
+export default MainPage;
