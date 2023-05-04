b64cbd72a092befa2f96666ef485276b3a200df0
matvey84 <matvey8407@mail.ru>
2023-05-04 23:13:10 +0300

    feat: add vacancy item

--- src/components/vacancy-list/VacancyList.tsx
+++ src/components/vacancy-list/VacancyList.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import VacancyItem from '../vacancy-item/VacancyItem';
 
 function VacancyList() {
-  return <section className="main-page_vacancy_list">item</section>;
+  return (
+    <section className="main-page_vacancy_list">
+      <VacancyItem />
+      <VacancyItem />
+      <VacancyItem />
+      <VacancyItem />
+    </section>
+  );
 }
 
 export default VacancyList;
