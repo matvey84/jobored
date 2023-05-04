6ffcf9929b43431ee514d14c43d70eaad0360c7a
matvey84 <matvey8407@mail.ru>
2023-05-04 12:45:01 +0300

    feat: add appRouter

--- src/appRouter/AppRouter.tsx
+++ src/appRouter/AppRouter.tsx
@@ -1,10 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from './Layout';
+import MainPage from '../pages/mainPage/MainPage';
+import FavoriteVacancyPage from '../pages/favoriteVacancyPage/FavoriteVacancyPage';
+import CurrentVacancyPage from '../pages/currentVacancy/CurrentVacancyPage';
 
 export default function AppRouter() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}></Route>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<MainPage />} />
+        <Route path="/favorite-vacancy" element={<FavoriteVacancyPage />} />
+        <Route path="/vacancy/:id" element={<CurrentVacancyPage />} />
+      </Route>
     </Routes>
   );
 }
--- src/appRouter/Layout.tsx
+++ src/appRouter/Layout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import Header from '../components/header/Header';
 
 export default function Layout() {
   return (
     <>
+      <Header />
       <Outlet />
     </>
   );
