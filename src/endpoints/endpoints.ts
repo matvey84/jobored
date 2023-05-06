5f23ea2a006fa52598a1bd7f8dbc8fcf35f19f3a
matvey84 <matvey8407@mail.ru>
2023-05-06 10:16:36 +0300

    feat: add files with request endpoints

---
+++ src/endpoints/endpoints.ts
@@ -0,0 +1,14 @@
+const URL = 'https://startup-summer-2023-proxy.onrender.com';
+// const URL = ' http://localhost:4000';
+export const Endpoints = {
+  ROOT: `${URL}`,
+  AUTH: `${URL}/2.0/oauth2/password`,
+  VACANCYES: `${URL}/2.0/vacancies`,
+
+  USERS: `${URL}/users`,
+  SIGN_IN: `${URL}/signin`,
+  SIGN_UP: `${URL}/signup`,
+  BOARDS: `${URL}/boards`,
+  FILE: `${URL}/file`,
+  LOGS: `${URL}/logs`,
+};
