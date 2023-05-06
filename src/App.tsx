7b292265bcfbd8d93e73761fad0ac1db923adb8c
matvey84 <matvey8407@mail.ru>
2023-05-06 10:16:02 +0300

    add:  userFetch Recuest

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import './App.css';
 import AppRouter from './appRouter/AppRouter';
+import { useAppDispatch } from './redux/hooks';
+import { USER } from './endpoints/mocUser';
+import { fetchLogin } from './redux/user-slice/userFetchRequest';
 
 function App() {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(fetchLogin(USER));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <Fragment>
       <AppRouter />
