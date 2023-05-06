0a4fc41655d2f171c63c5d739ba42ed288ae566b
matvey84 <matvey8407@mail.ru>
2023-05-06 10:19:04 +0300

    feat: add dataFtch request for get vacancies

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './mainPage.scss';
 import MainPageSearchForm from '../../components/main-page-search-form/MainPageSearchForm';
 import VacancyList from '../../components/vacancy-list/VacancyList';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import { fetchGetVacancy } from '../../redux/data-slice/dataFetchRequest';
 
 function MainPage() {
+  const dispatch = useAppDispatch();
+  const user = useAppSelector((state) => state.userSlice);
+  useEffect(() => {
+    dispatch(fetchGetVacancy(user));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <section className="page main-page">
       <div className="container main-page_container">
