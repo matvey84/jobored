5cb36975e262a822ebe5665b413e50b9a8ae07c4
matvey84 <matvey8407@mail.ru>
2023-05-08 20:59:36 +0300

    feat: add autorization request

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React, { Fragment, useEffect } from 'react';
 import './App.css';
 import AppRouter from './appRouter/AppRouter';
-import { useAppDispatch } from './redux/hooks';
+import { useAppDispatch, useAppSelector } from './redux/hooks';
 import { USER } from './endpoints/mocUser';
 import { fetchLogin } from './redux/user-slice/userFetchRequest';
 
 function App() {
   const dispatch = useAppDispatch();
+  const token = useAppSelector((state) => state.userSlice.access_token);
   useEffect(() => {
-    dispatch(fetchLogin(USER));
+    !!token && dispatch(fetchLogin(USER));
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
