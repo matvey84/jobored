90af9b01040ab3416672525f3804eebbdde709d3
matvey84 <matvey8407@mail.ru>
2023-05-10 23:42:10 +0300

    refactor: refactor code

--- src/redux/data-slice/dataFetchRequest.ts
+++ src/redux/data-slice/dataFetchRequest.ts
@@ -1,25 +1,30 @@
-import { IRootVacancyResponse, IVacansy } from './../../types/vacancyTypes';
+import { IFetchQueryVacancyRequest } from './../../types/requestTypes';
+import { IRootVacancyResponse } from './../../types/vacancyTypes';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { Endpoints } from '../../endpoints/endpoints';
-import { IUserState } from '../../types/sliceTypes';
+import { createrQueryString } from '../handlers/handlers';
 
-export const fetchGetVacancy = createAsyncThunk<IVacansy[], IUserState, { rejectValue: string }>(
-  'fetch/fetchGetVacancy',
-  async (user, { rejectWithValue }) => {
-    const response: Response = await fetch(Endpoints.VACANCYES, {
+export const fetchGetVacancy = createAsyncThunk<
+  IRootVacancyResponse,
+  IFetchQueryVacancyRequest,
+  { rejectValue: string }
+>('fetch/fetchGetVacancy', async (fetchQuery, { rejectWithValue }) => {
+  const response: Response = await fetch(
+    createrQueryString(fetchQuery.paginationData, Endpoints.VACANCYES),
+    {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
-        'X-Api-App-Id': user.user.client_secret,
+        'X-Api-App-Id': fetchQuery.x_api_app_id,
         'x-secret-key': 'GEU4nvd3rej*jeh.eqp',
       },
-    });
-
-    if (!response.ok) {
-      return rejectWithValue(`Somethig went wrong. Response end with ${response.status}`);
     }
+  );
 
-    const vacancyes: IRootVacancyResponse = await response.json();
-    return vacancyes.objects;
+  if (!response.ok) {
+    return rejectWithValue(`Somethig went wrong. Response end with ${response.status}`);
   }
-);
+
+  const vacancyes: IRootVacancyResponse = await response.json();
+  return vacancyes;
+});
