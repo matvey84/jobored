6979416e4dda96f63a2d88863891f8547ed9d784
matvey84 <matvey8407@mail.ru>
2023-05-15 10:53:01 +0300

    refactor: reafactor message condition

--- src/components/nofavoriteVanacy/NoFavoriteVacancyMessage.tsx
+++ src/components/nofavoriteVanacy/NoFavoriteVacancyMessage.tsx
@@ -2,19 +2,12 @@ import React from 'react';
 import './noFavoriteVacancy.scss';
 import NoFavoriteVacancySVG from '../../ui/NoFavoriteVacancySVG';
 import BackToVacancyButton from '../../ui/buttons/BackToVacancyButton ';
-import { useLocation } from 'react-router-dom';
 
 function NoFavoriteVacancyMessage() {
-  const location = useLocation();
-  const currentMessage = location.pathname.includes('favorite') ? (
-    <h1 className="message">Упс, здесь еще ничего нет!</h1>
-  ) : (
-    <h1 className="message">Упс, вакансия пермещена в архив!</h1>
-  );
   return (
     <section className="message_wrapper">
       <NoFavoriteVacancySVG />
-      <h1 className="message">{currentMessage}</h1>
+      <h1 className="message">Упс, здесь еще ничего нет!</h1>
       <BackToVacancyButton />
     </section>
   );
