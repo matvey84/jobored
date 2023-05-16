665c1a47d1f3752f2abbd7ba322cb3a3696cabd0
matvey84 <matvey8407@mail.ru>
2023-05-16 12:54:05 +0300

    feat: add condition for render vacancyes

--- src/components/vacancy-list/VacancyList.tsx
+++ src/components/vacancy-list/VacancyList.tsx
@@ -2,19 +2,26 @@ import React from 'react';
 import VacancyItem from '../vacancy-item/VacancyItem';
 import { nanoid } from '@reduxjs/toolkit';
 import { IVacansy } from '../../types/vacancyTypes';
+import NoFavoriteVacancyMessage from '../nofavoriteVanacy/NoFavoriteVacancyMessage';
+import Loader from '../loader/Loader';
 
 interface IProp {
   vacancyData: IVacansy[];
+  spinnerStatus?: boolean;
 }
 function VacancyList(props: IProp) {
-  const { vacancyData } = props;
+  const { vacancyData, spinnerStatus } = props;
 
   return (
     <div className="vacancy-list__wrapper">
       <section className="main-page_vacancy-list">
-        {vacancyData.map((vacancy) => (
-          <VacancyItem key={nanoid()} data={vacancy} />
-        ))}
+        {spinnerStatus ? (
+          <Loader />
+        ) : !vacancyData.length ? (
+          <NoFavoriteVacancyMessage />
+        ) : (
+          vacancyData.map((vacancy) => <VacancyItem key={nanoid()} data={vacancy} />)
+        )}
       </section>
     </div>
   );
