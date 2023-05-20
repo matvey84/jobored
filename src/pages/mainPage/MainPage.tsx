2e2250ef44a7a7778a135da828e2f79fdc724a89
matvey84 <matvey8407@mail.ru>
2023-05-20 22:23:20 +0300

    refactor: refactor mainpage

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -5,26 +5,36 @@ import VacancyList from '../../components/vacancy-list/VacancyList';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import { fetchGetVacancy } from '../../redux/data-slice/dataFetchRequest';
 import Paginator from '../../components/paginator/Paginator';
-import { IFetchQueryVacancyRequest } from '../../types/requestTypes';
-import {
-  setFirstAndLastPaginationPages,
-  setPaginationFetchQuery,
-} from '../../redux/paginator-slice/paginationStateSlice';
+import { IFetchQuery } from '../../types/requestTypes';
+import { setFirstAndLastPaginationPages } from '../../redux/paginator-slice/paginationStateSlice';
 import Loader from '../../components/loader/Loader';
-import { createAllButtonsNumberNote, queryString } from '../../redux/handlers/handlers';
+import { createAllButtonsNumberNote, queryString2 } from '../../redux/handlers/handlers';
+import FilterForm from '../../components/filter-form/FilterForm';
+import { useLocation } from 'react-router-dom';
 
 function MainPage() {
   const dispatch = useAppDispatch();
-  const token = useAppSelector((state) => state.dataSlice.access_token);
-  const user = useAppSelector((state) => state.userSlice.user);
-  const pageCounter = useAppSelector((state) => state.dataSlice.pageCount);
-  const paginationData = useAppSelector((state) => state.paginationStateSlice.paginationFetchQuery);
+  const location = useLocation();
   const spinnerStatus = useAppSelector((state) => state.dataSlice.spinnerStatus);
   const vacancyData = useAppSelector((state) => state.dataSlice.data);
   const totalVacancies = useAppSelector((state) => state.dataSlice.totalVacancies);
   const numIndex = useAppSelector((state) => state.paginationStateSlice.numIndex);
   const pagesAmmount = useAppSelector((state) => state.dataSlice.pagesAmount);
-  const searchVacancie = useAppSelector((state) => state.dataSlice.searchVacancie);
+
+  useEffect(() => {
+    const fetchQueryData: IFetchQuery = {
+      published: 1,
+      page: 1,
+      keyword: '',
+      catalogues: '',
+      payment_from: 0,
+      payment_to: 0,
+    };
+    !!location.search
+      ? dispatch(fetchGetVacancy(location.search))
+      : dispatch(fetchGetVacancy(queryString2(fetchQueryData)));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const buttonCreate = useCallback(() => {
     return createAllButtonsNumberNote(pagesAmmount, numIndex);
@@ -38,36 +48,10 @@ function MainPage() {
     dispatch(setFirstAndLastPaginationPages(firstAndLastPaginationPages));
   }, [buttonCreate, dispatch, numIndex]);
 
-  useEffect(() => {
-    const queryObject: IFetchQueryVacancyRequest = {
-      x_api_app_id: user.client_secret,
-      token,
-      paginationData: !!paginationData
-        ? paginationData
-        : {
-            page: 1,
-            count: pageCounter,
-          },
-    };
-
-    !!searchVacancie
-      ? dispatch(
-          fetchGetVacancy(
-            queryString({
-              keyword: searchVacancie,
-              page: String(paginationData.page),
-            })
-          )
-        )
-      : dispatch(fetchGetVacancy(queryString(queryObject.paginationData)));
-    dispatch(setPaginationFetchQuery(queryObject.paginationData));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
     <section className="page main-page">
       <div className="container main-page_container">
-        <form className="main-page_form-block"></form>
+        <FilterForm />
         <section className="main-page_vacancy-block">
           {spinnerStatus && <Loader />}
           <MainPageSearchForm />
